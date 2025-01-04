f481a5531aadc974583ec031d9a25f97381653ce
YashwanthK <yashwanthkothakota@gmail.com>
2025-01-04 11:30:25 +0530

    add ec2 instance and commented creation of key pair for reference

--- main.tf
+++ main.tf
@@ -8,6 +8,8 @@ variable subnet_cidr_block{}
 variable avail_zone{}
 variable env_prefix{} 
 variable my_ip{}
+variable instance_type{}
+variable public_key_location{}
 
 
 resource "aws_vpc" "myapp-vpc"{
@@ -121,5 +123,44 @@ resource "aws_security_group" "myapp-sg"{
 
     tags = {
         Name: "${var.env_prefix}-sg"
+    }
+}
+
+data "aws_ami" "latest-amazon-linux-image"{
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+output "aws_ami_id"{
+    value = data.aws_ami.latest-amazon-linux-image
+}
+
+# resource "aws_key_pair" "ssh-key"{
+#     key_name = "server-key"
+#     public_key = file(var.public_key_location)
+# }
+
+resource "aws_instance" "myapp-server"{
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = "server-key-pair"
+    # key_name = aws_key_pair.ssh-key.key_name
+
+    tags={
+        Name: "${var.env_prefix}-server"
     }
 }
