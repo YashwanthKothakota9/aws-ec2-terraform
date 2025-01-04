9f2c297a108d1290bcbaef31cdceff7494e00f1c
YashwanthK <yashwanthkothakota@gmail.com>
2025-01-04 10:40:17 +0530

    add new security group

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ variable vpc_cidr_block{}
 variable subnet_cidr_block{}
 variable avail_zone{}
 variable env_prefix{} 
+variable my_ip{}
 
 
 resource "aws_vpc" "myapp-vpc"{
@@ -44,7 +45,49 @@ resource "aws_route_table" "myapp-route-table"{
     }
 }
 
+# resource "aws_default_route_table" "main-rtb"{
+#     default_route_table_id = aws_vpc.myapp_vpc.default_route_table_id
+#     route{
+#         cidr_block = "0.0.0.0/0"
+#         gateway_id = aws_internet_gateway.myapp-igw.id
+#     }
+#     tags = {
+#         Name: "${var.env_prefix}-main-rtb"
+#     }
+# }
+
 resource "aws_route_table_association" "a-rtb-subnet"{
     subnet_id = aws_subnet.myapp-subnet-1.id
     route_table_id = aws_route_table.myapp-route-table.id
+}
+
+resource "aws_security_group" "myapp-sg"{
+    name = "myapp-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress{
+        from_port = 22
+        to_port = 22
+        protocol = "TCP"
+        cidr_blocks = [var.my_ip]
+    }
+
+    ingress{
+        from_port = 8080
+        to_port = 8080
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress{
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+
+    tags = {
+        Name: "${var.env_prefix}-sg"
+    }
 }
